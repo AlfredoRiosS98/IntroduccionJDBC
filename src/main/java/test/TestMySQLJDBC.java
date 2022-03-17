47b1a50662e26d6e60d16c2fa55ac2f5903cdbcc
AlfredoRiosS98 <alfredoriossilva12@gmail.com>
2022-03-16 22:51:10 -0700

    Finalizado

--- src/main/java/test/TestMySQLJDBC.java
+++ src/main/java/test/TestMySQLJDBC.java
@@ -7,12 +7,13 @@ public class TestMySQLJDBC {
         String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublickeyRetrieval=true";
         try {
             //Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conexion = DriverManager.getConnection(url, "nameUser", "password");
+            Connection conexion = DriverManager.getConnection(url, "AlfredoRiosS98", "Rios_12");
             Statement instruccion = conexion.createStatement();
             var sql = "SELECT id_personas, nombre, apellido, email, telefono FROM personas";
             ResultSet resultado = instruccion.executeQuery(sql);
             while(resultado.next()){
                 System.out.println("Id Persona" + resultado.getInt("id_personas"));
+                System.out.println("Nombre: " + resultado.getString("nombre"));
             }
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
